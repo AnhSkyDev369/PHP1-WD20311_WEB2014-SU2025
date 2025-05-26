ad02dfab0e17f662d0296a5c3f62b2a3c2007a02
anhntph36206 <anhntph36206@fpt.edu.vn>
2025-05-26 15:03:34 +0700

    Buoi 4 - 24-05-2025

---
+++ Buoi-04/buoi4_oop-1.php
@@ -0,0 +1,137 @@
+<?php
+
+class SinhVien
+{
+    // Khai báo các thuộc tính và phương thức của lớp SinhVien
+
+    // Cách khai báo thuộc tính (Khai báo biến tron Class)
+    // public(công khai) là phạm vi được phép truy cập, ở đâu cũng có thể truy cập được
+
+    public $hoTen;      // Thuộc tính tên của sinh viên
+    public $namSinh;    // Thuộc tính năm sinh của sinh viên
+    public $maSinhVien; // Thuộc tính mã sinh viên
+
+    //  Cách khai báo phương thức (Khai báo hàm trong class)
+    public function hienThi() {
+        // echo "Họ tên: $hoTen";   // $hoTen là 1 biến bình thường...
+        echo "Họ tên: $this->hoTen" . '<br/>';//  $this-> Gọi đến thuộc tính trong class
+        echo "Năm sinh: $this->namSinh" . '<br/>';
+        echo "Mã sinh viên: $this->maSinhVien" .'<br/>';
+    }
+}
+//  Class là 1 Khuôn mẫu (BluePrint) để tạo ra nhiều Đối tượng (Object)
+//  Các Object tạo ra sẽ có CHUNG thuộc tính và phương thức
+
+//  Object là các đối tượng được tạo ra từ Class
+$sinhVien1 = new SinhVien();//  Khởi tạo 1 Đối tượng, $sinhVien1 là tên đối tượng, có các thược tính, phương thức ...
+//  Một Class có thể tạo ra nhiều đối tượng nhưng
+//  một đối tượng chỉ thuộc về 1 Class
+
+//  Gán giá trị cho các thuộc tính
+//  Cách 1:
+$sinhVien1->hoTen = 'Nguyễn Văn A';
+$sinhVien1->namSinh = 2005;
+$sinhVien1->maSinhVien = "PH12345";
+
+$sinhVien1->hienThi();//    Sử dụng phương thức (hành động) của đối tượng
+echo '<br/>';
+//  BT: Khởi tạo thêm 3 đối tượng khác nhau từ class SinhVien
+$sinhVien2 = new SinhVien();
+$sinhVien2->hoTen = 'Nguyễn Văn B';
+$sinhVien2->namSinh = 2006;
+$sinhVien2->maSinhVien = "PH23456";
+$sinhVien2->hienThi();
+echo '<br/>';
+
+$sinhVien3 = new SinhVien();
+$sinhVien3->hoTen = 'Nguyễn Văn C';
+$sinhVien3->namSinh = 2007;
+$sinhVien3->maSinhVien = "PH34567";
+$sinhVien3->hienThi();
+echo '<br/>';
+
+$sinhVien4 = new SinhVien();
+$sinhVien4->hoTen = 'Nguyễn Văn D';
+$sinhVien4->namSinh = 2008;
+$sinhVien4->maSinhVien = "PH45678";
+$sinhVien4->hienThi();
+echo '<br/>';
+//  Một Class có thể được sử dụng ở nhiều nơi
+
+//  BT1:
+//  Khai báo class DongVat có các thuộc tính
+//  Tên, tuổi, cân nặng, giống loài, màu sắc, giới tính
+//  Có các phương thức: ăn, ngủ, di chuyển, thông tin
+//  Tạo thêm 1 phương thức hiển thị toàn bộ thông tin của đối tượng
+//  Khởi tạo 3 đối tượng khác nhau của class
+
+class DongVat 
+{
+    public $tenDongVat;
+    public $tuoiDV;
+    public $canNangDV;
+    public $giongDV;
+    public $mauSacDV;
+    public $gioiTinhDV;
+
+    public function anUong() {
+        echo "$this->tenDongVat đang ăn <br/>";
+    }
+
+    public function nguNghi() {
+        echo "$this->tenDongVat đang ngủ <br/>";
+    }
+
+    public function diChuyen() {
+        echo "$this->tenDongVat di chuyển bằng 4 chân.<br/>";
+    }
+
+    public function thongTin() {
+        echo "Tên: $this->tenDongVat <br/>";
+        echo "Tuổi: $this->tuoiDV <br/>";
+        echo "Cân nặng: $this->canNangDV <br/>";
+        echo "Giống loài: $this->giongDV <br/>";
+        echo "Màu sắc: $this->mauSacDV <br/>";
+        echo "Giới tính: $this->gioiTinhDV <br/>";
+    }
+
+    public function hienThiThongTin() {
+        echo "<hr/>";
+        echo "<strong>Thông tin chi tiết:</strong><br/>";
+        $this->thongTin();
+        $this->anUong();
+        $this->nguNghi();
+        $this->diChuyen();
+    }
+}
+
+$dongVat1 = new DongVat();
+$dongVat1->tenDongVat = "Con mèo";
+$dongVat1->tuoiDV = 10;
+$dongVat1->canNangDV = "6kg";
+$dongVat1->giongDV = "Mèo Mướp";
+$dongVat1->mauSacDV = "Trắng Đen";
+$dongVat1->gioiTinhDV = "Đực";
+
+
+$dongVat2 = new DongVat();
+$dongVat2->tenDongVat = 'Con chó';
+$dongVat2->tuoiDV = 5;
+$dongVat2->canNangDV = "12kg";
+$dongVat2->giongDV = "Chó Cỏ";
+$dongVat2->mauSacDV = "Xám Trắng";
+$dongVat2->gioiTinhDV = "Cái";
+
+$dongVat3 = new DongVat();
+$dongVat3->tenDongVat = 'Con bò';
+$dongVat3->tuoiDV = 8;
+$dongVat3->canNangDV = "250kg";
+$dongVat3->giongDV = "Bò Sữa";
+$dongVat3->mauSacDV = "Đen Trắng";
+$dongVat3->gioiTinhDV = "Cái";
+
+
+$dongVat1->hienThiThongTin();
+$dongVat2->hienThiThongTin();
+$dongVat3->hienThiThongTin();
+
---
+++ Buoi-04/buoi4_oop.php
@@ -0,0 +1,39 @@
+<?php
+//  OOP là viết tắt của lập trình hướng đối tượng
+//  LẬP TRÌNH HƯỚNG ĐỐI TƯỢNG TRONG OOP
+//  Lập trình hướng đối tượng là gì ?
+//  -   Là một kỹ thuật lập trình (Được sử dụng trong hầu hết các ngôn ngữ)
+//  -   Kỹ thuật lập trình đưa các bài toán thành 1 đối tượng cụ thể để giải quyết.
+
+//  ĐỐI TƯỢNG LÀ GÌ ?
+//  -   Đối tượng là một thực thể mà ta có thể thực hiện thao tác với
+
+//  
+//  Đối tượng là một thực thể trong thế giới thực mà chúng ta có thể nhìn thấy, cảm nhận và tương tác.
+//  Ví dụ: Con mèo, con chó, cái bàn, cái ghế, cái tủ lạnh, cái máy tính, cái điện thoại, cái xe máy, cái ô tô
+
+//  CÁC TÍNH CHẤT CỦA LẬP TRÌNH HƯỚNG ĐỐI TƯỢNG
+//  - Tính đóng gói
+//  - Tính kế thừa
+//  - Tính đa hình
+//  - Tính trừu tượng
+
+//  TÍNH ĐÓNG GÓI
+//  - Tính đóng gói sẽ được thể hiện thông qua Class(Lớp)
+//  - Class sẽ đóng gối tất cả thuộc tính và phương thức
+//  - Từ Class có thể tạo ra nhiều đối tượng có chung thuộc tính và phương thức trong Class
+//  - Thuộc tính: là những gì giúp cấu thành đối tượng (Biến)
+//  - Phương thức: là những hành động mà đối tượng có thể thực hiện (Hàm)
+
+//  - Class là một khuôn mẫu (BluePrint) để tạo ra các đối tượng
+//  - Thuộc tính (Properties) là các biến được định nghĩa trong Class,
+//    đại diện cho các đặc điểm của đối tượng.
+
+//  - Phương thức (Methods) là các hàm được định nghĩa trong Class,
+//    đại diện cho các hành động mà đối tượng có thể thực hiện.
+
+
+//  - Tính đóng gói (Encapsulation) là quá trình
+//   nhóm các thuộc tính và phương thức của một đối tượng lại với nhau, 
+//   ẩn đi các chi tiết bên trong và chỉ cung cấp các phương thức công khai để tương tác với đối tượng đó. 
+//   Điều này giúp bảo vệ dữ liệu và giảm thiểu sự phụ thuộc giữa các đối tượng.
